740d52ec004568a8ec26cd44b944bc9fc857e0ac
cong-coc-2612 <congcoicoc2612@gmail.com>
2020-09-03 20:27:43 +0700

    fix build

---
+++ build/precache-manifest.4c9a2050ef259b8ca53689e12f5c9c2e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6d45736a9404e8e5a48957a1e52141f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7be5a351092d3988773b",
+    "url": "/static/css/2.87e76f7e.chunk.css"
+  },
+  {
+    "revision": "7a3659bb6a93761c49b7",
+    "url": "/static/css/main.9307bc35.chunk.css"
+  },
+  {
+    "revision": "7be5a351092d3988773b",
+    "url": "/static/js/2.20d5bd16.chunk.js"
+  },
+  {
+    "revision": "cecf2e9beba7d75ee5fcedc3ab38544f",
+    "url": "/static/js/2.20d5bd16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a3659bb6a93761c49b7",
+    "url": "/static/js/main.05b582ab.chunk.js"
+  },
+  {
+    "revision": "65fc61b9f0aec17d5513",
+    "url": "/static/js/runtime-main.4d2f6ecc.js"
+  },
+  {
+    "revision": "d041e2a534491915a55d554fe1daf691",
+    "url": "/static/media/1.d041e2a5.jpg"
+  },
+  {
+    "revision": "9421a9187584959093d4996542d96834",
+    "url": "/static/media/38155584462_74d5f1cc1d_b.9421a918.jpg"
+  },
+  {
+    "revision": "15695b44d14846ad90c5245f99b3a2fd",
+    "url": "/static/media/7ab1b0125d485c8dd6a4e78832b0a4b2fbed3cf8.15695b44.png"
+  },
+  {
+    "revision": "8cd101f4343e39b9d4cb052251a8587d",
+    "url": "/static/media/image-384.8cd101f4.png"
+  },
+  {
+    "revision": "d27a1a365df377aa1276a6d7483c053f",
+    "url": "/static/media/logo.d27a1a36.png"
+  }
+]);
